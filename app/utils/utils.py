826b91d1677b6d0e1a66b6b71d1c3fadc91041d5
VVeb1250 <teethawat.k@kkumail.com>
2025-10-12 23:53:56 +0900

    local time and mod some code

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -1,11 +1,16 @@
-from datetime import date, timedelta
+import os
+import pytz
+from datetime import date, datetime
 from flask import session
+
+APP_TIMEZONE = os.getenv("APP_TIMEZONE", "UTC")
+TIMEZONE = pytz.timezone(APP_TIMEZONE)
+
 def get_today():
     """คืนวันที่จำลอง ถ้าไม่มีให้ใช้วันจริง"""
     if "simulated_date" in session:
         return date.fromisoformat(session["simulated_date"])
-    return date.today()
-
+    return datetime.now(TIMEZONE).date()
 
 import inspect
 import os
